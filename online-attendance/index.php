5f1ea84d5f0e66831ce9d5c1dbea63a83ad84024
Jamzed222 <jamzedbantog@gmail.com>
2025-03-26 13:38:17 +0800

    fix

--- online-attendance/index.php
+++ online-attendance/index.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  //test
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register & Login</title>
@@ -15,14 +14,12 @@
         <div class="input-group">
            <i class="fas fa-user"></i>
            <input type="text" name="fName" id="fName" placeholder="First Name" required>
-           <label for="fname">Firstname</label>
-           <br>
+           <label for="fname">First Name</label>
         </div>
         <div class="input-group">
             <i class="fas fa-user"></i>
             <input type="text" name="lName" id="lName" placeholder="Last Name" required>
             <label for="lName">Last Name</label>
-            <br>
         </div>
         <div class="input-group">
             <i class="fas fa-envelope"></i>
@@ -32,7 +29,7 @@
         <div class="input-group">
             <i class="fas fa-lock"></i>
             <input type="password" name="password" id="password" placeholder="Password" required>
-            <label for="password">Pasword</label>
+            <label for="password">Password</label>
         </div>
        <input type="submit" class="btn" value="Sign Up" name="signUp">
       </form>
